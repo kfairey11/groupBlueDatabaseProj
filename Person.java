8305ee562e8f180d42f826e219c9fc7a8d33690b
caeblue <caeblue11@gmail.com>
2021-03-10 20:46:03 -0500

    person and UserProfile

--- Person.java
+++ Person.java
@@ -1,3 +1,49 @@
-public class Person{
+public class Person
+{
+    protected String firstName;
+    protected String lastName;
+    protected int age;
+    protected Sex sex;
+
+    public String getFirstName()
+    {
+        return "";
+    }
+
+    public void setFirstName(String aFirstName)
+    {
+        //empty stub
+    }
+
+    public String getLastName()
+    {
+        return "";
+    }
+
+    public void setLastName(String aLastName)
+    {
+        //empty stub
+    }
+
+    public int getAge()
+    {
+        return 0;
+    }
     
+    public void setAge(int anAge)
+    {
+        //empty stub
+    }
+
+    public Sex getSex()
+    {
+        return null;
+    }
+
+    public void setSex(Sex sex)
+    {
+        //empty stub
+    }
+
+
 }
