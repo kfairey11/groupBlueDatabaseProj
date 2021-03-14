2e9015009cecd4e43e4614e248eb65dc7a46daac
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2021-03-13 22:03:54 -0500

    created dataConstants

---
+++ DataConstants.java
@@ -0,0 +1,41 @@
+public abstract class DataConstants {
+    protected final static String USER_FILE_NAME = "src/User.json";
+    protected final static String USER_ID = "id";
+    protected final static String USER_FIRST_NAME = "firstName";
+    protected final static String USER_LAST_NAME = "lastName";
+
+    protected final static String CRIMINAL_FILE_NAME = "src/Criminal.json";
+    protected final static String CRIMINAL_FIRST_NAME = "firstName";
+    protected final static String CRIMINAL_LAST_NAME = "lastName";
+    protected final static String CRIMINAL_AGE = "age";
+    protected final static String CRIMINAL_SEX = "sex";
+    protected final static String CRIMINAL_NICKNAME = "nickname";
+    protected final static String CRIMINAL_HEIGHT = "height";
+    protected final static String CRIMINAL_WEIGHT = "weight";
+    protected final static String CRIMINAL_RACE = "race";
+    protected final static String CRIMINAL_TATTOOS = "tattoos";
+    protected final static String CRIMINAL_SHOE_SIZE = "shoeSize";
+    protected final static String CRIMINAL_PIERCINGS = "piercings";
+
+    protected final static String VICTIM_FILE_NAME = "src/Victim.json";
+    protected final static String VICTIM_ID = "id";
+    protected final static String VICTIM_FIRST_NAME = "firstName";
+    protected final static String VICTIM_LAST_NAME = "lastName";
+    protected final static String VICTIM_AGE = "age";
+    protected final static String VICTIM_SEX = "sex";
+    protected final static String VICTIM_REPORT = "report";
+    protected final static String VICTIM_CASE_NUM = "caseNum";
+    protected final static String VICTIM_HOSPITAL = "hospital";
+    protected final static String VICTIM_PHONE_NUM = "phoneNum";
+    protected final static String VICTIM_ADDRESS = "address";
+    protected final static String VICTIM_PROTECTIVE_CUSTODY = "protectedCustody";
+
+    protected final static String OFFICER_FILE_NAME = "src/Officer.json";
+    protected final static String OFFICER_FIRST_NAME = "firstName";
+    protected final static String OFFICER_LAST_NAME = "lastName";
+    protected final static String OFFICER_AGE = "age";
+    protected final static String OFFICER_SEX = "sex";
+
+
+ }
+
