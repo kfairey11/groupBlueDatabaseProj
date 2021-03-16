9d22a3ae6175740d900489b82db0061aedf3cad1
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2021-03-15 23:49:38 -0400

    finished haveCase method

---
+++ Cases.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Cases {
+    private Cases cases;
+    private ArrayList<Case> caseList;
+
+    private Cases()
+    {
+        cases = Database.getCases();
+    }
+
+    public Cases getInstance()
+    {
+        if(cases == null)
+            cases = new Cases();
+        return cases;
+    }
+
+    public boolean haveCase(int caseNum)
+    {
+        for(int i=0; i<caseList.size(); i++)
+        {
+            if(caseNum == caseList.get(i).getCaseNum())
+        }
+    }
+}
