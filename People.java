0672260030c186bb8d693452a3c4c8cec2b4a9e6
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2021-03-15 23:23:43 -0400

    created People.java

---
+++ People.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class People {
+    private static People people;
+    private ArrayList<Person> personList;
+
+    private People()
+    {
+        personList = Database.getPeople();
+    }
+
+    public static People getInstance()
+    {
+        if(people == null)
+            people = new People();
+        return people;
+    }
+
+    public boolean havePerson(String name)
+    {
+        for(int i=0;i<personList.size();i++)
+        {
+            String fullName = ((personList.get(i)).getFirstName() + " " + (personList.get(i)).getLastName());
+            if(name.equalsIgnoreCase(fullName))
+                return true;
+        }
+        return false;
+    }
+
+    //possibly getItem()?
+}
