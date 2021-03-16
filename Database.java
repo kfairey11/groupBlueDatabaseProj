f353e32d440be5772ef5d31891a09dc7230a668d
kfairey11 <77368958+kfairey11@users.noreply.github.com>
2021-03-15 23:06:17 -0400

    created database class with arrayLists of user, people, cases

--- Database.java
+++ Database.java
@@ -1,118 +1,19 @@
 import java.util.*;
 public class Database {
     
-    private static ArrayList<Person> people;
-    private static ArrayList<Case> cases;
-    private static Database database;
-
-    private Database(ArrayList<Person> people, ArrayList<Case> cases)
-    {}
-
-    public static Database getInstance()
+    public static ArrayList<User> getUsers()
     {
-        //creates one database
-        if(database == null) 
-            database = new Database(people, cases);
-        return database;
+        return new ArrayList<User>();
+    }
+    
+    public static ArrayList<Person> getPeople()
+    {
+        return new ArrayList<Person>();
     }
 
-    public void createCriminal(String firstName, String lastName, int age, String sex, String nickname, int feet, int inches,
-    double weight, String race, String[] tattoos, double shoeSize, String[] piercings)
+    public static ArrayList<Case> getCases()
     {
-        //adds criminal to the database based on this info
-        //need to convert height info into the height format
-        //need to add all elements of string arrays into arraylists
+        return new ArrayList<Case>();
     }
-
-    public void createVictim(String firstName, String lastName, int age, String sex, String report, String hospital, int phoneNum, 
-    String address, boolean protCust)
-    {
-        //adds victim to the database
-    }
-
-    public void createOfficer(String firstName, String lastName, int age, String sex, String rank,String city, int phoneNum, String address)
-    {
-        //adds officer to the database
-    }
-
-    public void createWitness(String firstName, String lastName, int age, String sex, String testimony, int phoneNum, String address)
-    {
-        //adds witness to the database
-    }
-
-    public void createCase(String crimeType, String date, String description, String location)
-    {
-        //adds a case to the database
-    }
-    public void changeCriminal(Person criminal)
-    {
-        //empty stub
-    }
-
-    public void changeVictim(Person victim)
-    {
-        //empty stub
-    }
-
-    public void changeOfficer(Person officer)
-    {
-        //empty stub
-    }
-
-    public void changeWitness(Person witness)
-    {
-        //empty stub
-    }
-
-    public void changeCase(Case caseToChange)
-    {
-        //empty stub
-    }
-    public Criminal searchCriminalByLastName(String name)
-    {
-        //empty stub
-        return null;
-    }
-
-    public Victim searchVictimByLastName(String name)
-    {
-        //empty stub
-        return null;
-    }
-
-    public Officer searchOfficerByLastName(String name)
-    {
-        //empty stub
-        return null;
-    }
-
-    public Witness searchWitnessByLastName(String name)
-    {
-        //empty stub 
-        return null;
-    }
-
-    public void searchPerson()
-    {
-        //empty stub
-    }
-
-    public void searchCase()
-    {
-        //empty stub
-    }
-
-    public Case searchCaseByCaseNum(int caseNum)
-    {
-        //empty stub
-        return null;
-    }
-
-
-    public void addToCase(Case newCase)
-    {
-        //empty stub
-    }
-
 
 }
