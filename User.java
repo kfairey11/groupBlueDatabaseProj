c90d2206fdd2f37431c94552311e835ed8940650
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 01:45:34 -0400

    Added javadoc to User

--- User.java
+++ User.java
@@ -1,64 +1,119 @@
+/**
+ * A User Class
+ * @Author Casey, Kennedy
+ */
 import java.util.*;
 
 public class User {
+    /**
+     * Attributes of a User
+     */
     private UUID userID;
     private String firstName;
     private String lastName;
     private String userName;
     private String password;
 
+    /**
+     * Constructor for a User
+     * @param userID
+     * @param firstName
+     * @param lastName
+     * @param userName
+     * @param password
+     */
     public User(UUID userID, String firstName, String lastName, String userName, String password){
         //empty stub
     }
 
-        public UUID getUserID()
-        {
-            return userID;
-        }
+    /**
+     * Gets the userID
+     * @return userID
+     */
+    public UUID getUserID()
+    {
+        return userID;
+    }
 
-        public void setUserID(UUID userID)
-        {
-            this.userID = userID;
-        }
+    /**
+     * Sets the userID
+     * @param userID
+     */
+    public void setUserID(UUID userID)
+    {
+        this.userID = userID;
+    }
 
-        public String getFirstName()
-        {
-            return firstName;
-        }
+    /**
+     * Gets the first name of the User
+     * @return
+     */
+    public String getFirstName()
+    {
+        return firstName;
+    }
 
-        public void setFirstName(String firstName)
-        {
-            this.firstName = firstName;
-        }
+    /**
+     * Sets the first name of the User
+     * @param firstName
+     */
+    public void setFirstName(String firstName)
+    {
+       this.firstName = firstName;
+    }
 
-        public String getLastName()
-        {
-            return lastName;
-        }
+    /**
+     * Gets the last name of the User
+     * @return last name 
+     */
+    public String getLastName()
+    {
+        return lastName;
+    }
 
-        public void setLastName(String lastName)
-        {
-            this.lastName = lastName;
-        }
+    /**
+     * Sets the last name for the User
+     * @param lastName
+     */
+    public void setLastName(String lastName)
+    {
+       this.lastName = lastName;
+    }
 
-        public String getUserName()
-        {
-            return userName;
-        }
+    /**
+     * Gets the user name of the User
+     * @return user name
+     */
+    public String getUserName()
+    {
+        return userName;
+    }
 
-        public void setUserName(String userName)
-        {
-            this.userName = userName;
-        }
+    /**
+     * Sets the user name for the User
+     * @param userName
+     */
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
 
-        public String getPassword()
-        {
-            return password;
-        }
+    /**
+     * Gets the password of the User
+     * @return password
+     */
+    public String getPassword()
+    {
+        return password;
+    }
 
-        public void setPassword(String password)
-        {
-            this.password = password;
-        }
+    /**
+     * Sets the password for the User
+     * @param password
+     */
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 
     }
