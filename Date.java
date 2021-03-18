7d234b75251f99cbc5fe639d320a1a7fff28913c
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 02:16:55 -0400

    Added javadoc to Date

--- Date.java
+++ Date.java
@@ -1,16 +1,34 @@
+/**
+ * A Date Class
+ * @author Casey, Kennedy
+ */
 public class Date extends Case
 {
+    /**
+     * Attributes for the Date
+     */
     private int day;
     private int month;
     private int year;
 
+    /**
+     * Constructor for the Date
+     * @param aDay
+     * @param aMonth
+     * @param aYear
+     */
     public Date(int aDay, int aMonth, int aYear)
     {
+        
         day= aDay;
         month=aMonth;
         year=aYear;
     }
 
+    /**
+     * Prints out the Date
+     * @return string describing the Date
+     */
     public String toString()
     {
         return "Month: " + month + " Day: " + day + " Year: " + year + "\n" + month + "/" + day + "/" + year;
