f36d4aa9480830cdcbdffacca89a7346e1f21b99
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 02:26:40 -0400

    Added javadoc to Database

--- Database.java
+++ Database.java
@@ -1,16 +1,32 @@
+/**
+ * A Database Class
+ * @author Kennedy
+ */
 import java.util.*;
 public class Database {
     
+    /**
+     * Gets a list of Users
+     * @return ArrayList<User>
+     */
     public static ArrayList<User> getUsers()
     {
         return new ArrayList<User>();
     }
     
+    /**
+     * Gets a list of Person
+     * @return ArrayList<Person>
+     */
     public static ArrayList<Person> getPeople()
     {
         return new ArrayList<Person>();
     }
 
+    /**
+     * Gets a list of Cases
+     * @return ArrayList<Case>
+     */
     public static ArrayList<Case> getCases()
     {
         return new ArrayList<Case>();
