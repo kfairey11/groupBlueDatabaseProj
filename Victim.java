df4c260f36361a6daf6ebd9a32dd8b4234276c18
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 01:56:58 -0400

    Added javadoc to Victim

--- Victim.java
+++ Victim.java
@@ -1,11 +1,29 @@
+/**
+ * A Victim Class
+ * @author Casey, Kennedy
+ */
 public class Victim extends Person
 {
+    /**
+     * Attributes of a Victim
+     */
     private String report;
     private Case caseLink;
     private String hospital;
     private int phoneNum;
     private String address;
 
+    /**
+     * Constructor for a Victim
+     * @param firstName
+     * @param lastName
+     * @param age
+     * @param sex
+     * @param aReport
+     * @param aHospital
+     * @param aPhoneNum
+     * @param anAddress
+     */
     public Victim(String firstName, String lastName, int age, String sex, String aReport, String aHospital, int aPhoneNum, String anAddress)
     {
         super(firstName, lastName, age, sex);
@@ -15,11 +33,19 @@ public class Victim extends Person
         address=anAddress;
     }
 
+    /**
+     * Gets Victim report
+     * @return report
+     */
     public String getReport()
     {
         return report;
     }
 
+    /**
+     * Sets the report of the Victim
+     * @param aReport
+     */
     public void setReport(String aReport)
     {
         if(aReport.equalsIgnoreCase(" "))
@@ -32,12 +58,19 @@ public class Victim extends Person
         }
     }
 
-
+    /**
+     * Gets the hospital of the victim
+     * @return hospital
+     */
     public String getHopsital()
     {
         return hospital;
     }
 
+    /**
+     * Sets the hospital of the victim
+     * @param aHospital
+     */
     public void setHospital(String aHospital)
     {
         if(aHospital.equalsIgnoreCase(" "))
@@ -50,11 +83,19 @@ public class Victim extends Person
         }
     }
 
+    /**
+     * Gets the phone number of the victim
+     * @return phoneNum
+     */
     public int getPhoneNum()
     {
         return phoneNum;
     }
 
+    /**
+     * Sets the phone number of the victim
+     * @param aPhoneNum
+     */
     public void setPhoneNum(int aPhoneNum)
     {
         if(phoneNum > 0)
@@ -67,11 +108,19 @@ public class Victim extends Person
         }
     }
 
+    /**
+     * Gets the address of the victim
+     * @return address
+     */
     public String getAddress()
     {
         return address;
     }
 
+    /**
+     * Sets the address of the Victim
+     * @param anAddress
+     */
     public void setAddress(String anAddress)
     {
         if(anAddress.equalsIgnoreCase(" "))
@@ -83,7 +132,4 @@ public class Victim extends Person
             anAddress=anAddress;
         }
     }
-
-
-    
 }
