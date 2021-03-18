b6d747318a478458189f2f2321d6b8d9a127a26e
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 01:32:15 -0400

    added javadoc to Criminal

--- Criminal.java
+++ Criminal.java
@@ -1,6 +1,13 @@
+/**
+ * A Criminal Class
+ * @author Sam, Casey, Kennedy
+ */
 import java.util.ArrayList;
 
 public class Criminal extends Person{
+    /**
+     * Attributes of a Criminal
+     */
     private String nickname;
     private Height height;
     private Double weight;
@@ -10,6 +17,11 @@ public class Criminal extends Person{
     private ArrayList<String> piercing;
     private boolean isUnderAge; //needs a check for this
 
+    /**
+     * Criminal Constructor
+     * @param String nickname, Height height, double weight, String race, 
+     * @param ArrayList tattoos, ArrayList piercings, double shoesize, boolean isUnderAge
+     */
     public Criminal(String nickName, Height aheight, double aweight, String arace,
     ArrayList<String> atattoos, ArrayList<String> apiercing, double shoesize, boolean IsUnderAge){
         this.nickname = nickName;
@@ -22,10 +34,18 @@ public class Criminal extends Person{
         this.isUnderAge = IsUnderAge;
     }
 
+    /**
+     * Gets the nickname of the criminal
+     * @return string of nickname
+     */
     public String getNickname() {
         return nickname;
     }
 
+    /**
+     * Sets the nickname of the criminal
+     * @param aNickname
+     */
     public void setNickname(String aNickname) {
         if(aNickname.equalsIgnoreCase(" ")) {
             nickname = nickname;
@@ -34,28 +54,52 @@ public class Criminal extends Person{
         }
     }
 
+    /**
+     * Gets the height of the criminal
+     * @return height
+     */
     public Height getHeight() {
         return height;
     }
 
+    /**
+     * Sets the height of the criminal
+     * @param aHeight
+     */
     public void setHeight(Height aHeight){
         height = aHeight;
     }
 
+    /**
+     * Gets the weight of the criminal
+     * @return weight
+     */
     public double getWeight() {
         return weight;
     }
 
+    /**
+     * Sets the weight of the criminal
+     * @param aWeight
+     */
     public void setWeight(double aWeight) {
         if (aWeight > 0.0) {
             weight = aWeight;
         }
     }
     
+    /**
+     * Gets the race of the criminal
+     * @return race
+     */
     public String getRace(){
         return race;
     }
 
+    /**
+     * Sets the race of the criminal
+     * @param aRace
+     */
     public void setRace(String aRace) {
         if(aRace.equalsIgnoreCase(" ")) {
             race = race;
@@ -64,28 +108,52 @@ public class Criminal extends Person{
         }
     }
 
+    /**
+     * Gets the list of tattoos on the criminal
+     * @return tattoos
+     */
     public ArrayList<String> getTattoos() {
         return tattoos;
     }
 
+    /**
+     * Sets the list of tattoos on the criminal
+     * @param aTattoos
+     */
     public void setTattos(ArrayList<String> aTattoos){
         tattoos = aTattoos;
     }
 
+    /**
+     * Gets the shoe size of the criminal
+     * @return shoe size
+     */
     public double getShoeSize() {
         return shoeSize;
     }
 
+    /**
+     * Sets the shoe size of the criminal
+     * @param aShoeSize
+     */
     public void setShoeSize(double aShoeSize) {
         if (aShoeSize > 0.0) {
             shoeSize = aShoeSize;
         }
     }
 
+    /**
+     * Gets the list of piercings on the criminal
+     * @return list of piercings
+     */
     public ArrayList<String> getPiercing() {
         return piercing;
     }
 
+    /**
+     * Sets the list of piercings on the criminal
+     * @param aPiercing
+     */
     public void setPiercing(ArrayList<String> aPiercing) {
         piercing = aPiercing;
     }
