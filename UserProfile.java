ce87deb8c10a13a500e6371a5e5b4a08bd29bcb2
Sam Lowry <lowrysa@email.sc.edu>
2021-03-18 02:06:24 -0400

    Added javadoc to UserProfile

--- UserProfile.java
+++ UserProfile.java
@@ -1,9 +1,20 @@
+/**
+ * A UserProfile Class
+ * @author Aubrey, Casey
+ */
 public class UserProfile 
 {
+    /**
+     * Attributes of the UserProfile
+     */
     protected String username;
     protected String password;
     private User user;
     
+    /**
+     * Creates a User Profile
+     * @param aUser
+     */
     public void createUser(User aUser)
     {
         this.username = aUser.getUserName();
@@ -11,6 +22,10 @@ public class UserProfile
         this.user = aUser;
     }
 
+    /**
+     * Gets a User
+     * @return User
+     */
     public User getUser()
     {
         
