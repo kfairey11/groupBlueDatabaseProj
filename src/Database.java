75fb2fc67c5dc0ff55777b0fd45aabbb4bd2b383
aubrey-addy <aaaddy@email.sc.edu>
2021-03-27 19:36:14 -0400

    added JavaDoc to Database

--- src/Database.java
+++ src/Database.java
@@ -15,23 +15,36 @@ public class Database {
     }
     
     /**
-     * Gets a list of Person
-     * @return ArrayList<Person>
+     * Gets a list of Criminals
+     * @return ArrayList<Criminal>
      */
     public static ArrayList<Criminal> getCriminals()
     {
         return new ArrayList<Criminal>();
     }
 
+    /**
+     * Gets a list of Victims
+     * @return ArrayList<Victim>
+     */
     public static ArrayList<Victim> getVictims()
     {
         return new ArrayList<Victim>();
     }
 
+    /**
+     * Gets a list of Officers
+     * @return ArrayList<Officer>
+     */
     public static ArrayList<Officer> getOfficers()
     {
         return new ArrayList<Officer>();
     }
+
+    /**
+     * Gets a list of Witnesses
+     * @return ArrayList<Witness>
+     */
     public static ArrayList<Witness> getWitnesses()
     {
         return new ArrayList<Witness>();
