aa3f923b88a159ef10185f11a0b5008d3059d69c
Sam Lowry <lowrysa@email.sc.edu>
2021-04-11 01:33:30 -0400

    updating stuff

--- src/PeopleTest.java
+++ src/PeopleTest.java
@@ -2,6 +2,42 @@
  * People Tester
  * @author Sam Lowry
  */
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.UUID;
 public class PeopleTest {
-    
+    private People people = People.getInstance();
+    private ArrayList<Criminal> criminalList = people.getCriminalList();
+    private ArrayList<Victim> victimList = people.getVictimList();
+    private ArrayList<Officer> officerList = people.getOfficerList();
+    private ArrayList<Witness> witnessList = people.getWitnessList();
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+    }
+
+    @Test
+
+
+
+    @Test
+
+
+
+    @Test
+
+
+
+    @Test
 }
