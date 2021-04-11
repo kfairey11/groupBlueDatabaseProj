8f4d4cf2a0bf46879aa0f8e4b7a5b34318425bf7
Sam Lowry <lowrysa@email.sc.edu>
2021-04-11 14:43:00 -0400

    updated tests

--- src/PeopleTest.java
+++ src/PeopleTest.java
@@ -26,33 +26,33 @@ public class PeopleTest {
 
     @BeforeEach
     public void setup() {
-        criminalList.clear();
-        criminalList.add(new Criminal("Joe", "Bob", 34, "Male", "Ay its Joe", "6'2\"", 110.4, "white", "brown", "brown", "very skinny", array("fairy", "umbrella"), 11.5, array("left ear", "right ear"), false));
-        criminalList.add(new Criminal("Bob", "Joe", 68, "Male", "Bob to the Joe", "6'0\"", 190.8, "white", "black", "blue", "very large", array("bear", "mom surrounded by heart"), 11.0, array("mouth", "nose"), false));
+        this.criminalList.clear();
+        this.criminalList.add(new Criminal("Joe", "Bob", 34, "Male", "Ay its Joe", "6'2\"", 110.4, "white", "brown", "brown", "very skinny", array("fairy", "umbrella"), 11.5, array("left ear", "right ear"), false));
+        this.criminalList.add(new Criminal("Bob", "Joe", 68, "Male", "Bob to the Joe", "6'0\"", 190.8, "white", "black", "blue", "very large", array("bear", "mom surrounded by heart"), 11.0, array("mouth", "nose"), false));
         DataWriter.saveCriminals();
 
-        victimList.clear();
-        victimList.add(new Victim("Hanna", "Fini", 20, "Female", "Was used as a hostage", "none", Long.parseLong("8435555911"), "45 Scottsdale Drive"));
-        victimList.add(new Victim("Thomas", "Bobson", 43, "Male", "Was stabbed", "Wolfsons", Long.parseLong("8035553472"), "154 Bermuda Greens Ave"));
+        this.victimList.clear();
+        this.victimList.add(new Victim("Hanna", "Fini", 20, "Female", "Was used as a hostage", "none", Long.parseLong("8435555911"), "45 Scottsdale Drive"));
+        this.victimList.add(new Victim("Thomas", "Bobson", 43, "Male", "Was stabbed", "Wolfsons", Long.parseLong("8035553472"), "154 Bermuda Greens Ave"));
         DataWriter.saveVictims();
 
-        officerList.clear();
-        officerList.add(new Officer("Rebecca", "Robinson", 56, "Female", "Chief", "Seattle, Washington", Long.parseLong("7435558174"), "395 Pandora Drive"));
-        officerList.add(new Officer("Bo", "Burnher", 30, "Male", "Detective", "Jacksonville, Florida", Long.parseLong("9045551294"), "1203 Franklin Street"));
+        this.officerList.clear();
+        this.officerList.add(new Officer("Rebecca", "Robinson", 56, "Female", "Chief", "Seattle, Washington", Long.parseLong("7435558174"), "395 Pandora Drive"));
+        this.officerList.add(new Officer("Bo", "Burnher", 30, "Male", "Detective", "Jacksonville, Florida", Long.parseLong("9045551294"), "1203 Franklin Street"));
         DataWriter.saveOfficers();
 
-        witnessList.clear();
-        witnessList.add(new Witness("Brady", "McBrady", 25, "Male", "I saw someone steal a purse", Long.parseLong("3825559238"), "9128 Forest Drive"));
-        witnessList.add(new Witness("Tom", "Roberto", 34, "Male", "I saw someone get shot", Long.parseLong("4395551935"), "34 Idaho Avenue"));
+        this.witnessList.clear();
+        this.witnessList.add(new Witness("Brady", "McBrady", 25, "Male", "I saw someone steal a purse", Long.parseLong("3825559238"), "9128 Forest Drive"));
+        this.witnessList.add(new Witness("Tom", "Roberto", 34, "Male", "I saw someone get shot", Long.parseLong("4395551935"), "34 Idaho Avenue"));
         DataWriter.saveWitnesses();
     }
 
     @AfterEach
     public void tearDown() {
-        people.getCriminalList().clear();
-        people.getVictimList().clear();
-        people.getOfficerList().clear();
-        people.getWitnessList().clear();
+        this.people.getCriminalList().clear();
+        this.people.getVictimList().clear();
+        this.people.getOfficerList().clear();
+        this.people.getWitnessList().clear();
         DataWriter.saveCriminals();
         DataWriter.saveVictims();
         DataWriter.saveOfficers();
